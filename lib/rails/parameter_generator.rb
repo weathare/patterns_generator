17013892a6ac233ff71d546c0ef66b158432c9f6
weathare <fuzuki.close@gmail.com>
2017-03-29 14:04:40 +0900

    inject method to controller

--- lib/rails/parameter_generator.rb
+++ lib/rails/parameter_generator.rb
@@ -5,4 +5,24 @@ class Rails::ParameterGenerator < Rails::Generators::NamedBase
     template "parameter.erb", File.join("app/parameters", class_path, "#{file_name}_parameter.rb")
     template "parameter_spec.erb", File.join("spec/parameters", class_path, "#{file_name}_parameter_spec.rb")
   end
+
+  # @note only when first generatedd
+  def inject_code
+    apps_file = Rails.root.join(*%w[app controllers application_controller.rb])
+
+    inject_into_file apps_file, before: /^end/ do <<-"CODE"
+
+  private
+    def permitted_params_class
+      controller = params.permit(:controller)[:controller]
+      controller << "_parameter"
+      @permitted_params_class ||= controller.camelize.constantize
+    end
+
+    def permitted_params
+      @permitted_params ||= permitted_params_class.request_params(params)
+    end
+    CODE
+    end unless self.behavior == :invoke && File.read(apps_file) =~ /permitted_params_class/
+  end
 end
--- spec/rails/parameter_generator_spec.rb
+++ spec/rails/parameter_generator_spec.rb
@@ -3,14 +3,31 @@ require "rails_helper"
 RSpec.describe Rails::ParameterGenerator, type: :generator do
   destination File.expand_path("../../tmp", __FILE__)
 
-  before { prepare_destination }
+  before do
+    prepare_destination
+    FileUtils.cp(
+      Rails.root.join(*%w[app controllers application_controller.rb]),
+      destination_root
+    )
+  end
+
+  after do
+    FileUtils.cp(
+      destination_root + "/application_controller.rb",
+      Rails.root.join(*%w[app controllers application_controller.rb])
+    )
+    prepare_destination
+  end
 
   context "generate files" do
     before { run_generator %w(api/resources/regist) }
 
     it { expect(file("app/parameters/api/resources/regist_parameter.rb")).to exist }
     it { expect(file("spec/parameters/api/resources/regist_parameter_spec.rb")).to exist }
+    it(:inject_code) do
+      file = File.read(Rails.root.join(*%w[app controllers application_controller.rb]))
+      expect(file).to match /permitted_params_class/
+      expect(file).to match /permitted_params/
+    end
   end
-
-  after(:context) { FileUtils.rm_rf(destination_root) }
 end
