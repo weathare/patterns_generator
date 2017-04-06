31bc26e4bbdd952ae94cf40b572e88bdca8d82b0
weathare <fuzuki.close@gmail.com>
2017-04-06 11:40:54 +0900

    rename module filename

--- lib/generators/concern/model/model_generator.rb
+++ lib/generators/concern/model/model_generator.rb
@@ -2,7 +2,7 @@ class Concern::ModelGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../../templates", __FILE__)
 
   def create_concerns
-    template "model.erb", File.join("app/models/concerns", class_path, "#{file_name}.rb")
+    template "base.erb", File.join("app/models/concerns", class_path, "#{file_name}.rb")
     template "model_spec.erb", File.join("spec/models/concerns", class_path, "#{file_name}_spec.rb")
   end
 end
